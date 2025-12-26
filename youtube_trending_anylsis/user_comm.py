ece20b4abec06d6f9a7e8e7594f0a542af761694
Adilrage <2514603@live.stmarys.ac.uk>
2025-12-26 18:31:04 +0000

    Delete youtube_trending_anylsis directory

--- youtube_trending_anylsis/user_comm.py
+++
@@ -1,8 +0,0 @@
-# user_comm.py
-def display_menu() -> str:
-    print("Menu")
-    print("1. Load data")
-    print("2. Show top categories")
-    print("3. Show top channels by views")
-    print("0. Exit")
-    return input("Choose: ").strip()
